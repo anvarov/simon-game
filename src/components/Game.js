0177d620db825223203cc2b27bf4ae3b366dd11d
anvarov <akmalanvaroff@gmail.com>
2019-01-23 06:20:42 -0600

    added basic simon game board

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import "../styles/styles.scss";
 
 class Game extends Component {
   constructor(props) {
     super(props);
     this.onClick = e => {
-			return props.dispatch({
-				type: "SET_VOLUME_UP",
-				id: e
-			});
+      return props.dispatch({
+        type: "SET_VOLUME_UP",
+        id: e
+      });
     };
 
     this.onClick = this.onClick.bind(this);
@@ -17,20 +18,24 @@ class Game extends Component {
 
   render() {
     return (
-      <div onClick={this.onClick}>
-        hi there
+      <div className="container">
+        <div id="red" />
+        <div id="red_background"></div>
+        <div id="blue" />
+        <div id="blue_background"></div>
+        <div id="menu"></div>
+        <div id="yellow" />
+        <div id="yello_background"></div>
+        <div id="green" />
+        <div id="green_background"></div>
       </div>
     );
   }
 }
-const mapStateToProps = (state) => ({
-		settings: state.settings
-	
-})
+const mapStateToProps = state => ({
+  settings: state.settings
+});
 
-const mapDispatchToProps = {
-	
-}
-
+const mapDispatchToProps = {};
 
 export default connect(mapStateToProps)(Game);
